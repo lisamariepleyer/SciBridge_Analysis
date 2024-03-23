6b4ab1844f1497cd51434a81f7482025b2840a60
lisamariepleyer <lisaminusmarie.p@gmail.com>
2024-03-23 12:55:37 +0100

    parse raw data to somethining programmatically meaningful

--- scripts/01_parse_raw_data.R
+++ scripts/01_parse_raw_data.R
@@ -6,3 +6,17 @@ quizstarts <- fread('raw_data/SciBridge - quizstarts.csv')
 questions <- fread('raw_data/SciBridge - questions.csv')
 personal_infos <- fread('raw_data/SciBridge - personalinfos.csv')
 
+# parse Ja, Nein, keine Angabe and NA values to something programmatically meaningful
+
+for (col in c("isCorrect", "hasViewedSource", "hasViewedGame")) {
+  questions[is.na(get(col)), eval(col) := FALSE]
+}
+
+for (col in c("hasUsedSources", "hasUsedGoogle")) {
+  personal_infos[get(col) == "Ja", eval(col) := TRUE]
+  personal_infos[get(col) == "Nein", eval(col) := FALSE]
+}
+
+for (col in c("age", "gender")) {
+  personal_infos[get(col) == "keine Angabe", eval(col) := NA]
+}
